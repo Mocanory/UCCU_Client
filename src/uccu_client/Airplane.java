cc9b45fc87788a3396c38a6de5b1dc187ad1078c
Mocanory <Mocanory@mocanory>
2015-06-13 20:13:28 +0800

    加入了攻击函数

--- src/uccu_client/Airplane.java
+++ src/uccu_client/Airplane.java
@@ -15,4 +15,8 @@ public class Airplane extends Entity {
 		this.speed=5;	//初始速度先设为1
 		// TODO 最后两个参数size和angle先传固定值，以后再加
 	}
+	public Warhead attack(Airplane target){
+		Warhead tmp = new Warhead(this.posX, this.posY, target);
+		return tmp;
+	}
 }
